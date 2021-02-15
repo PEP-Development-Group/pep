69c768dca782e08389978ca971556181d0585157
zKen <690898835@qq.com>
2021-02-15 22:04:18 +0800

    Update cls_class.go

--- server/router/cls_class.go
+++ server/router/cls_class.go
@@ -9,20 +9,21 @@ import (
 func InitClassRouter(Router *gin.RouterGroup) {
 	ClassRouter := Router.Group("class").Use(middleware.OperationRecord())
 	{
-		ClassRouter.POST("createClass", v1.CreateClass)              // 新建Class
-		ClassRouter.DELETE("deleteClass", v1.DeleteClass)            // 删除Class
-		ClassRouter.DELETE("deleteClassByIds", v1.DeleteClassByIds)  // 批量删除Class
-		ClassRouter.PUT("updateClass", v1.UpdateClass)               // 更新Class
-		ClassRouter.GET("findClass", v1.FindClass)                   // 根据ID获取Class
-		ClassRouter.GET("getClassList", v1.GetClassListWithPerson)   // 学生选课Class列表
+		ClassRouter.POST("createClass", v1.CreateClass)             // 新建Class
+		ClassRouter.DELETE("deleteClass", v1.DeleteClass)           // 删除Class
+		ClassRouter.DELETE("deleteClassByIds", v1.DeleteClassByIds) // 批量删除Class
+		ClassRouter.PUT("updateClass", v1.UpdateClass)              // 更新Class
+		ClassRouter.GET("findClass", v1.FindClass)                  // 根据ID获取Class
+
+		ClassRouter.GET("getClassList", v1.GetClassListWithPerson)   // 学生选课Class列表（树状api）
 		ClassRouter.GET("getClass", v1.GetClassList)                 // Class管理列表
-		ClassRouter.GET("getPersonalClasses", v1.GetPersonalClasses) // 获取个人课表
+		ClassRouter.GET("getPersonalClasses", v1.GetPersonalClasses) // 学生获取个人课表
 
 		ClassRouter.GET("getTeacherClassList", v1.GetTeacherClassList)         // 教师获取个人课表
 		ClassRouter.GET("getTeacherAClassStuList", v1.GetTeacherAClassStuList) // 教师某课所有学生情况
-		ClassRouter.PATCH("setStuGrade", v1.SetStuGrade) // 教师某课所有学生情况
+		ClassRouter.PATCH("setStuGrade", v1.SetStuGrade)                       // 修改学生成绩（只能一次）
 
-		ClassRouter.POST("selectClass", v1.SelectClass)     // 选课
-		ClassRouter.DELETE("deleteSelect", v1.DeleteSelect) // 退选
+		ClassRouter.POST("selectClass", v1.SelectClass)     // 学生选课
+		ClassRouter.DELETE("deleteSelect", v1.DeleteSelect) // 学生退选
 	}
 }
