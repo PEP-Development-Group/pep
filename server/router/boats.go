19e468ad09db9194f272e5420bdfd4a38739c124
zKen <690898835@qq.com>
2021-02-17 11:14:48 +0800

    Update boats.go

--- server/router/boats.go
+++ server/router/boats.go
@@ -9,11 +9,8 @@ func InitGlobalRouter(Router *gin.RouterGroup) {
 	GlobalRouter := Router.Group("board")
 	{
 		GlobalRouter.POST("createRecord", v1.CreateRecord) // 新建留言板记录
-		//GlobalRouter.DELETE("deleteBoats", v1.DeleteBoats)           // 删除Boats
-		//GlobalRouter.DELETE("deleteBoatsByIds", v1.DeleteBoatsByIds) // 批量删除Boats
-		GlobalRouter.PUT("updateRecord", v1.UpdateBoard) // 更新Boats
-		//GlobalRouter.GET("findBoats", v1.FindBoats)                  // 根据ID获取Boats
-		GlobalRouter.GET("getRecord", v1.GetRecord)   // 获取记录
-		GlobalRouter.DELETE("delRecord", v1.DeleteBoard) // 删除留言
+		GlobalRouter.PUT("updateRecord", v1.UpdateBoard)   // 更新Boats
+		GlobalRouter.GET("getRecord", v1.GetRecord)        // 获取记录
+		GlobalRouter.DELETE("delRecord", v1.DeleteBoard)   // 删除留言
 	}
 }
