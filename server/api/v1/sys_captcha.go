2ffa546e8f023cca6936663d8138edbc49c24f3c
zKen <690898835@qq.com>
2021-02-19 23:27:35 +0800

    减少验证码有效时间，更新readme

--- server/api/v1/sys_captcha.go
+++ server/api/v1/sys_captcha.go
@@ -6,9 +6,10 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/mojocn/base64Captcha"
 	"go.uber.org/zap"
+	"time"
 )
 
-var store = base64Captcha.DefaultMemStore
+var store = base64Captcha.NewMemoryStore(5120, 5*time.Minute)
 
 // @Tags Base
 // @Summary 生成验证码
@@ -18,7 +19,7 @@ var store = base64Captcha.DefaultMemStore
 // @Success 200 {string} string "{"success":true,"data":{},"msg":"验证码获取成功"}"
 // @Router /base/captcha [post]
 func Captcha(c *gin.Context) {
-	//字符,公式,验证码配置
+	// 字符,公式,验证码配置
 	// 生成默认数字的driver
 	driver := base64Captcha.NewDriverDigit(global.GVA_CONFIG.Captcha.ImgHeight, global.GVA_CONFIG.Captcha.ImgWidth, global.GVA_CONFIG.Captcha.KeyLong, 0.7, 80)
 	cp := base64Captcha.NewCaptcha(driver, store)
