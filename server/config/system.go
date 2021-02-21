f979d45f2c0e14366629659fdbf4a19d621af744
zKen <690898835@qq.com>
2021-02-21 14:38:12 +0800

    ip速率限制+first_day

--- server/config/system.go
+++ server/config/system.go
@@ -7,6 +7,6 @@ type System struct {
 	OssType       string `mapstructure:"oss-type" json:"ossType" yaml:"oss-type"`
 	UseMultipoint bool   `mapstructure:"use-multipoint" json:"useMultipoint" yaml:"use-multipoint"`
 
-	FirstDay   string `mapstructure:"first-day" json:"first-day" yaml:"first-day"`
+	FirstDay   string `mapstructure:"first_day" json:"first_day" yaml:"first_day"`
 	CancelNums int    `mapstructure:"cancel_nums" json:"cancel_nums" yaml:"cancel_nums"`
 }
---
+++ server/middleware/ratelimit_ip.go
@@ -0,0 +1,29 @@
+package middleware
+
+import (
+	"gin-vue-admin/constant"
+	"gin-vue-admin/global"
+	"github.com/gin-gonic/gin"
+	"time"
+)
+
+// 限制单IP每秒请求速率
+func RateLimitByIP() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ip := c.ClientIP()
+		if in, _ := global.GVA_REDIS.Get(ip).Result(); in != "" {
+			c.AbortWithStatusJSON(7, constant.TooMany)
+			return
+		}
+
+		if cnt, _ := global.GVA_REDIS.Get(ip).Int(); cnt >= 10 {
+			c.AbortWithStatusJSON(7, constant.TooMany)
+			global.GVA_REDIS.Set("black-"+ip, "exist", 3*time.Second)
+		} else if cnt == 0 {
+			global.GVA_REDIS.Set(ip, 0, 10*time.Second)
+		}
+
+		global.GVA_REDIS.Incr(ip)
+		c.Next()
+	}
+}
