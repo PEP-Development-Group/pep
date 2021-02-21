2779c97c2951277cc859b09b904d6f722193e19d
zKen <690898835@qq.com>
2021-02-21 14:12:46 +0800

    更新菜单时删除缓存

--- server/middleware/ratelimit.go
+++ server/middleware/ratelimit.go
@@ -4,8 +4,8 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// 限制单用户每秒请求速率
-func RateLimit() gin.HandlerFunc {
+// 限制单IP每秒请求速率
+func RateLimitByIP() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
 		c.Next()
