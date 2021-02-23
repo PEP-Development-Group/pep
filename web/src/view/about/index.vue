1bbd2e5d845416935263e855192a21d947288831
zKen <690898835@qq.com>
2021-02-23 21:41:49 +0800

    Update index.vue

--- web/src/view/about/index.vue
+++ web/src/view/about/index.vue
@@ -1,130 +1,131 @@
 <template>
-  <div>
-    <el-row :gutter="10">
-      <el-col
-          :xs="{ span: 24, offset: 0 }"
-          :sm="{ span: 24, offset: 0 }"
-          :md="{ span: 12, offset: 0 }"
-      >
-        <el-card style="margin-top: 20px">
-          <div slot="header">前端</div>
-          <div>
-            <el-row>
-              <el-col :span="6">
-                <a href="https://shuangxunian.gitee.io/">
-                  <el-avatar
-                      :src="imgwyl"
-                      class="org-img"
-                      shape="square"
-                  ></el-avatar>
-                </a>
-              </el-col>
-              <el-col :span="18" style="margin-top: 16px">
-                <b>霜序廿</b>
-                <p>shuangxunian@gmail.com</p>
-                <p>公者千古，私者一时</p>
-              </el-col>
-            </el-row>
+    <div class="container">
+        <h1 class="header">Designers and Developers.</h1>
+        <el-card class="p">
+            <el-avatar :src="imgljk" class="org-img" shape="square"></el-avatar>
+            <div class="right">
+                <span class="desc">Name: &nbsp;&nbsp;&nbsp; wasabi</span>
+                <br />
 
-            <el-divider></el-divider>
+                <span class="desc">
+                    Blog: &nbsp;&nbsp;&nbsp; https://kcode.icu/
+                </span>
+                <br />
 
-            <el-row>
-              <el-col :span="6">
-                <a href="">
-                  <el-avatar
-                      :src="imgdjh"
-                      class="org-img"
-                      shape="square"
-                  ></el-avatar>
-                </a>
-              </el-col>
-              <el-col :span="18" style="margin-top: 16px">
-                <b>BladeHiker</b>
-                <p>bladehiker@outlook.com</p>
-                <p>这里写一段废话</p>
-              </el-col>
-            </el-row>
-          </div>
+                <span class="desc">
+                    Discriptor: &nbsp; &nbsp; &nbsp; He is a gopher!
+                </span>
+            </div>
         </el-card>
-      </el-col>
 
-      <el-col
-          :xs="{ span: 24, offset: 0 }"
-          :sm="{ span: 24, offset: 0 }"
-          :md="{ span: 12, offset: 0 }"
-      >
-        <el-card style="margin-top: 20px">
-          <div slot="header">后端</div>
-          <div>
-            <el-row>
-              <el-col :span="6">
-                <a href="">
-                  <el-avatar
-                      :src="imgljk"
-                      class="org-img"
-                      shape="square"
-                  ></el-avatar>
-                </a>
-              </el-col>
-              <el-col :span="18" style="margin-top: 16px">
-                <b>sh1luo</b>
-                <p>silo1999@163.com</p>
-                <p>这里也写一段废话</p>
-              </el-col>
-            </el-row>
-          </div>
+        <el-card class="p"
+            ><el-avatar
+                :src="imgdjh"
+                class="org-img"
+                shape="square"
+            ></el-avatar>
+            <div class="right">
+                <span class="desc">Name: &nbsp;&nbsp;&nbsp; BladeHiker</span>
+                <br />
+
+                <span class="desc">
+                    Blog: &nbsp;&nbsp;&nbsp; https://kcode.icu/
+                </span>
+                <br />
+
+                <span class="desc">
+                    Discriptor: &nbsp; &nbsp; &nbsp; He is a gopher!
+                </span>
+            </div>
         </el-card>
-      </el-col>
-    </el-row>
-  </div>
+
+        <el-card class="p"
+            ><el-avatar
+                :src="imgwyl"
+                class="org-img"
+                shape="square"
+            ></el-avatar>
+            <div class="right">
+                <span class="desc">Name: &nbsp;&nbsp;&nbsp; 霜序廿</span>
+                <br />
+
+                <span class="desc">
+                    Blog: &nbsp;&nbsp;&nbsp; https://kcode.icu/
+                </span>
+                <br />
+
+                <span class="desc">
+                    Discriptor: &nbsp; &nbsp; &nbsp; He is a gopher!
+                </span>
+            </div>
+        </el-card>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "About",
-  data() {
-    return {
-      imgwyl: require("@/assets/imgwyl.jpg"),
-      imgdjh: require("@/assets/imgdjh.jpg"),
-      imgljk: require("@/assets/imgljk.jpg"),
-    };
-  },
-  methods: {},
+    name: "About",
+    data() {
+        return {
+            imgwyl: require("@/assets/imgwyl.jpg"),
+            imgdjh: require("@/assets/imgdjh.jpg"),
+            imgljk: require("@/assets/imgljk.jpg"),
+        };
+    },
+    methods: {},
 };
 </script>
 
 <style scoped>
-.el-card {
-  text-align: center;
+.p {
+    padding-bottom: 15px;
+    margin-bottom: 15px;
 }
 
-.el-card b {
-  font-size: x-large;
-  font-weight: lighter;
+.container {
+    padding: 60px;
 }
 
-.el-card p {
-  font-size: small;
-  line-height: normal;
-}
-
-.avatar-img {
-  float: left;
-  height: 40px;
-  width: 40px;
-  border-radius: 50%;
-  -webkit-border-radius: 50%;
-  -moz-border-radius: 50%;
-  margin-top: 15px;
+.desc {
+    margin-left: 30px;
+    font-size: 20px;
+    margin-bottom: 50px;
 }
 
 .org-img {
-  height: 100px;
-  width: 100px;
+    height: 100px;
+    width: 100px;
+    float: left;
+}
+
+.header {
+    font-size: 30px;
+    font-weight: 800;
+    margin-bottom: 30px;
+}
+
+.el-card b {
+    font-size: x-large;
+    font-weight: lighter;
+}
+
+.el-card p {
+    font-size: small;
+    line-height: normal;
+}
+
+.avatar-img {
+    float: left;
+    height: 40px;
+    width: 40px;
+    border-radius: 50%;
+    -webkit-border-radius: 50%;
+    -moz-border-radius: 50%;
+    margin-top: 15px;
 }
 
 .dom-center {
-  margin-left: 50%;
-  transform: translateX(-50%);
+    margin-left: 50%;
+    transform: translateX(-50%);
 }
 </style>
