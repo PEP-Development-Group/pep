b95d60e17d3c77df978e1a35c807cb28a20c7261
zKen <690898835@qq.com>
2021-02-26 22:39:41 +0800

    更新已知问题
    1. 去掉model.class.time字段，计算上课时间通过first day+周数*7+星期数
    2. 教师上完成绩后若>=60，则学生已修学时加上课程学时

--- server/model/sys_user.go
+++ server/model/sys_user.go
@@ -7,17 +7,17 @@ import (
 
 type SysUser struct {
 	global.GVA_MODEL
-	UUID     uuid.UUID `json:"uuid" gorm:"comment:用户UUID"`
-	Username string    `json:"username" gorm:"type:varchar(20);comment:学号/工号"`
+	UUID     uuid.UUID `json:"uuid,omitempty" gorm:"comment:用户UUID"`
+	Username string    `json:"username,omitempty" gorm:"type:varchar(20);comment:学号/工号"`
 	Password string    `json:"-" gorm:"type:varchar(32);comment:登录密码"`
-	Name     string    `json:"name" gorm:"type:varchar(10);default:系统用户;comment:姓名"`
+	Name     string    `json:"name,omitempty" gorm:"type:varchar(10);default:系统用户;comment:姓名"`
 
 	Authority   SysAuthority `json:"authority" gorm:"foreignKey:AuthorityId;references:AuthorityId;comment:用户角色"`
 	AuthorityId string       `json:"authorityId" gorm:"default:888;comment:用户角色ID"`
 
-	CancelNums int    `json:"cancel_nums" gorm:"default:3;comment:取消次数"`
-	Class      string `json:"class" gorm:"varchar(10);comment:班级"`
+	CancelNums int    `json:"cancel_nums,omitempty" gorm:"default:3;comment:取消次数"`
+	Class      string `json:"class,omitempty" gorm:"varchar(10);comment:班级"`
 
-	HaveCredits  int `json:"have_credits" gorm:"comment:已修学时"`
-	TotalCredits int `json:"total_credits" gorm:"comment:应修学时"`
+	HaveCredits  int `json:"have_credits,omitempty" gorm:"comment:已修学时"`
+	TotalCredits int `json:"total_credits,omitempty" gorm:"comment:应修学时"`
 }
