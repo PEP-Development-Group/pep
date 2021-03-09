4fe03332f3a4c93021b5714826b5a3c45d6cfc5e
zKen <690898835@qq.com>
2021-03-09 12:19:47 +0800

    Update tokenbucket.go

--- server/initialize/tokenbucket.go
+++ server/initialize/tokenbucket.go
@@ -6,6 +6,6 @@ import (
 )
 
 func InitTokenBucket() *ratelimit.Bucket {
-	// 瞬时最大可能3000人，系统每秒最多1000人左右
+	// 瞬时最大可能1500人，每人每秒平均5r，系统每秒最多处理1000人，5000请求左右
 	return ratelimit.NewBucketWithQuantum(time.Second, 15000, 5000)
 }
